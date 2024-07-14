e741a8130c8b9c38e2de59e4763a2fa4d8084d26
vivek6201 <gvicky4567888@gmail.com>
2024-07-14 22:22:22 +0530

    updated files

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -62,7 +62,6 @@ export const loginController = async (req: Request, res: Response) => {
 
     res.cookie("jwt", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
@@ -141,7 +140,6 @@ export const signupController = async (req: Request, res: Response) => {
 
     res.cookie("jwt", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000,
     });
     return res.status(201).json({
